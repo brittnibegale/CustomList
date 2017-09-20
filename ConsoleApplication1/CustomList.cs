4c730f8cf40af85a04a38df09165e7333cb53b3c
brittnibegale <brittnibegale@gmail.com>
2017-09-20 13:04:25 -0500

    adding solution and projects

---
+++ ConsoleApplication1/CustomList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomList
+{
+    public class CustomList<T>
+    {
+        T[] item;
+        public T this[int i]
+        {
+            get { return item[i]; }
+            set { item[i] = value; }
+        }
+
+        public CustomList()
+        {
+            item = new T[3];
+        }
+
+        public void Add(T item)
+        {
+
+        }
+
+    }
+}
---
+++ CustomListTests/CustomListTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomList;
+
+namespace CustomListTests
+{
+    [TestClass]
+    public class CustomListTest
+    {
+        [TestMethod]
+        public void Add_OneNumber_ReturnNumber()
+        {
+            //Arrange
+            int expected = 16;
+            CustomList<int> list = new CustomList<int>();
+
+            //Act
+            list.Add(16);
+
+            // Assert
+            Assert.AreEqual(expected, list[0]);
+        }
+
+        [TestMethod]
+        public void 
+    }
+}
